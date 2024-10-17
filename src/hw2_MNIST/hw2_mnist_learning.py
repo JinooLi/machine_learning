219b9f96eb5911babcf52b7aeb1daedf38b63077
jinoo <jinu1423@naver.com>
2024-10-17 23:13:48 +0000

    색상 반전 추가, 사용 기기 종류 명시(gpu,cpu), plot 개선

--- src/hw2_MNIST/hw2_mnist_learning.py
+++ src/hw2_MNIST/hw2_mnist_learning.py
@@ -3,21 +3,32 @@ import torch.nn as nn
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 from matplotlib import pyplot as plt
+import numpy as np
 import os
 
 # 데이터셋을 다운로드할 경로 설정
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 # use nvidia gpu
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-device = torch.device("cuda")
-print(device)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("This computer uses", device)
+
+
+# 사용자 정의 색 반전 클래스 정의
+# 랜덤으로 색상을 반전시키는 클래스
+class RandomInvertColor(object):
+    def __call__(self, tensor):
+        if torch.rand(1) > 0.5:
+            return tensor
+        return 1 - tensor  # 색상 반전
+
 
 # 데이터 전처리 정의
 transform = transforms.Compose(
     [
         transforms.ToTensor(),  # 이미지를 (0,1) 범위의 텐서로 변환
-        transforms.Normalize((0.5,), (0.5,)),
+        RandomInvertColor(),  # 이미지의 색상을 랜덤으로 반전
+        transforms.Normalize(mean=(0.5,), std=(0.5,)),  # 이미지를 정규화한다.
     ]
 )
 
@@ -35,12 +46,10 @@ test_dataset = datasets.MNIST(
 train_loader = DataLoader(train_dataset, batch_size=1024, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=1024, shuffle=False)
 
+# 데이터셋 확인
 examples = enumerate(train_loader)
 batch_idx, (example_data, example_targets) = next(examples)
-
-print(
-    f"예시 이미지 배치 크기: {example_data.size()}"
-)  # 예: torch.Size([64, 1, 28, 28])
+print(f"예시 이미지 배치 크기: {example_data.size()}")
 print(f"예시 라벨: {example_targets}")
 
 
@@ -76,15 +85,23 @@ optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
 # 학습 루프
 history = []
-for epoch in range(100):
+accuracy_mean = []
+history_test = []
+test_accuracy_mean = []
+for epoch in range(50):
     model.train()
+    total = 0
+    total_loss = 0
+    total_accuracy = 0
     for data, target in train_loader:
         data = data.to(device)
         target = target.to(device)
 
+        # target을 one-hot encoding으로 변환
         target = torch.nn.functional.one_hot(target, num_classes=10)
         target = target.type(torch.float32)
 
+        # 모델의 gradient를 초기화
         optimizer.zero_grad()
         output = model(data)
 
@@ -92,20 +109,74 @@ for epoch in range(100):
         loss.backward()
         optimizer.step()
 
+        total_loss += loss.item()
+
         numpy_output = output.detach().cpu().numpy()
         numpy_target = target.detach().cpu().numpy()
         numpy_output = numpy_output.argmax(axis=1)
         numpy_target = numpy_target.argmax(axis=1)
         correct_count = (numpy_output == numpy_target).sum()
         accuracy = correct_count / len(numpy_output)
-        print(f"Accuracy: {accuracy}")
+        total_accuracy += accuracy
 
-    history.append(loss.item())
-    plt.plot(history)
+        total += 1
+
+    history.append(total_loss / total)
+    accuracy_mean.append(total_accuracy / total)
+
+    # 한 epoch마다 test_data로 모델 평가
+    model.eval()
+    test_loss = 0
+    total = 0
+    total_accuracy = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data = data.to(device)
+            target = target.to(device)
+
+            target = torch.nn.functional.one_hot(target, num_classes=10)
+            target = target.type(torch.float32)
+
+            output = model(data)
+            test_loss += criterion(output, target).item()
+            total += 1
+
+            numpy_output = output.detach().cpu().numpy()
+            numpy_target = target.detach().cpu().numpy()
+            numpy_output = numpy_output.argmax(axis=1)
+            numpy_target = numpy_target.argmax(axis=1)
+            correct_count = (numpy_output == numpy_target).sum()
+            accuracy = correct_count / len(numpy_output)
+            total_accuracy += accuracy
+
+    history_test.append(test_loss / total)
+    test_accuracy_mean.append(total_accuracy / total)
+
+    # loss, accuracy 그래프 출력
+    plt.figure(figsize=(14, 6))
+    plt.tight_layout()
+
+    linspace = np.linspace(1, len(history), len(history))
+    plt.subplot(1, 2, 1)
+    plt.plot(linspace, history)
+    plt.plot(linspace, history_test)
     plt.xlabel("Epoch")
     plt.ylabel("Loss")
     plt.title("Training Loss")
-    plt.savefig("loss.png")
+    plt.legend(["train", "test"])
+
+    plt.subplot(1, 2, 2)
+    plt.plot(linspace, accuracy_mean)
+    plt.plot(linspace, test_accuracy_mean)
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+    plt.title("Training Accuracy")
+    plt.legend(["train", "test"])
+
+    plt.savefig("loss.png", dpi=100)
     plt.close()
 
-    print(f"Epoch {epoch+1}, Loss: {loss.item():.4f}")
+    print(f"Epoch {epoch+1}, Accuracy: {test_accuracy_mean[-1]:.4f}")
+
+# 모델의 가중치 저장
+torch.save(model.state_dict(), "model_weights.pth")
