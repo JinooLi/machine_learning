5f9eaa11de64bf8542532ce3921e4b729dda2c63
jinoo <jinu1423@naver.com>
2024-10-23 13:41:49 +0000

    구조 최적화, 경계선 추출 전처리 추가

--- src/hw2_MNIST/hw2_mnist_learning.py
+++ src/hw2_MNIST/hw2_mnist_learning.py
@@ -2,10 +2,19 @@ import torch
 import torch.nn as nn
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
+from torchsummary import summary
+from torchvision import datasets, transforms
 from matplotlib import pyplot as plt
 import numpy as np
 import os
 import time
+
+# 데이터셋을 다운로드할 경로 설정
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+# use nvidia gpu
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("This computer uses", device)
 
 
 # 파라미터 클래스 정의
@@ -31,53 +40,113 @@ class params:
         self.loss = loss
 
 
+param = params()
+
+
+class MnistTrain(nn.Module):
+    def __init__(self):
+        super(MnistTrain, self).__init__()
+        self.layer1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        self.layer2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.layer3 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.layer4 = nn.Dropout2d(p=0.25)
+        self.layer5 = nn.Linear(64 * 14 * 14, 128)
+        self.layer6 = nn.Dropout(p=0.5)
+        self.layer7 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = torch.relu(self.layer1(x))
+        x = torch.relu(self.layer2(x))
+        x = torch.relu(self.layer3(x))
+        x = torch.relu(self.layer4(x))
+        x = x.view(x.size(0), -1)
+        x = torch.relu(self.layer5(x))
+        x = torch.relu(self.layer6(x))
+        x = torch.softmax(self.layer7(x), dim=1)
+        return x
+
+
+# 사용자 정의 색 반전 클래스 정의
+# 랜덤으로 색상을 반전시키는 클래스
+class RandomInvertColor(object):
+    def __call__(self, tensor):
+        if torch.rand(1) > 0.5:
+            return tensor
+        return 1 - tensor  # 색상 반전
+
+
+# 모든 데이터셋을 컨투어로 변환하는 클래스 정의
+class ToContour(object):
+    def __call__(self, tensor):
+        tensor = (tensor > 0.5).float()
+        tensor = tensor.squeeze().numpy()
+        contour = np.zeros_like(tensor)
+        for i in range(1, tensor.shape[0] - 1):
+            for j in range(1, tensor.shape[1] - 1):
+                if tensor[i, j] == 1:
+                    if (
+                        tensor[i - 1, j] == 0
+                        or tensor[i + 1, j] == 0
+                        or tensor[i, j - 1] == 0
+                        or tensor[i, j + 1] == 0
+                    ):
+                        contour[i, j] = 1
+        return torch.tensor(contour).unsqueeze(0)
+
+
+# 데이터 전처리 정의
+transform = transforms.Compose(
+    [
+        transforms.ToTensor(),  # 이미지를 (0,1) 범위의 텐서로 변환
+        RandomInvertColor(),  # 이미지의 색상을 랜덤으로 반전
+        ToContour(),  # 이미지를 컨투어로 변환
+        transforms.Normalize(
+            mean=(param.normalize_mean,), std=(param.normalize_std,)
+        ),  # 이미지를 정규화한다.
+    ]
+)
+
+# 학습 및 테스트 데이터셋 다운로드
+train_dataset = datasets.MNIST(
+    root="./data", train=True, download=True, transform=transform
+)
+
+test_dataset = datasets.MNIST(
+    root="./data", train=False, download=True, transform=transform
+)
+
+# 이미지 출력
+img_loader = DataLoader(train_dataset, batch_size=9, shuffle=True)
+images, labels = next(iter(img_loader))
+
+fig, axes = plt.subplots(3, 3, figsize=(10, 10))
+fig.suptitle("train_dataset", fontsize=16)
+
+for i in range(9):
+    ax = axes[i // 3, i % 3]
+    ax.imshow(images[i].squeeze(), cmap="gray")
+    ax.set_title(f"Label: {labels[i]}")
+    ax.axis("off")
+
+plt.savefig("train_dataset.png", dpi=200)
+
+# 모델 구조 출력
+summary(MnistTrain(), (1, 28, 28))
+
+
 for ii in range(3):
-    # 파라미터 객체 생성
-    param = params(
-        name="Exp5-" + str(ii + 1), normalize_mean=0.1307, normalize_std=0.3081
-    )
-
+    # 실험 이름 설정
+    param.name = "Exp4-" + str(ii + 1)
     print(param.name)
-
-    # 데이터셋을 다운로드할 경로 설정
-    os.chdir(os.path.dirname(os.path.realpath(__file__)))
-
-    # use nvidia gpu
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print("This computer uses", device)
-
-    # 사용자 정의 색 반전 클래스 정의
-    # 랜덤으로 색상을 반전시키는 클래스
-    class RandomInvertColor(object):
-        def __call__(self, tensor):
-            if torch.rand(1) > 0.5:
-                return tensor
-            return 1 - tensor  # 색상 반전
-
-    # 데이터 전처리 정의
-    transform = transforms.Compose(
-        [
-            transforms.ToTensor(),  # 이미지를 (0,1) 범위의 텐서로 변환
-            RandomInvertColor(),  # 이미지의 색상을 랜덤으로 반전
-            transforms.Normalize(
-                mean=(param.normalize_mean,), std=(param.normalize_std,)
-            ),  # 이미지를 정규화한다.
-        ]
-    )
-
-    # 학습 및 테스트 데이터셋 다운로드
-    train_dataset = datasets.MNIST(
-        root="./data", train=True, download=True, transform=transform
-    )
-
-    test_dataset = datasets.MNIST(
-        root="./data", train=False, download=True, transform=transform
-    )
 
     # 데이터셋을 batch 단위로 불러올 수 있는 DataLoader 객체 생성.
     # DataLoader는 데이터셋을 배치 단위로 분할하고, 데이터를 섞어준다.
-    train_loader = DataLoader(train_dataset, batch_size=param.batch_size, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=1024, shuffle=False)
+    train_loader = DataLoader(
+        train_dataset, batch_size=param.batch_size, shuffle=True, num_workers=4
+    )
+    test_loader = DataLoader(
+        test_dataset, batch_size=1024, shuffle=False, num_workers=4
+    )
 
     # 데이터셋 확인
     examples = enumerate(train_loader)
@@ -85,32 +154,13 @@ for ii in range(3):
     print(f"예시 이미지 배치 크기: {example_data.size()}")
     print(f"예시 라벨: {example_targets}")
 
-    class MnistTrain(nn.Module):
-        def __init__(self):
-            super(MnistTrain, self).__init__()
-            self.layer1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
-            self.layer2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
-            self.layer3 = nn.MaxPool2d(kernel_size=2, stride=2)
-            self.layer4 = nn.Dropout2d(p=0.25)
-            self.layer5 = nn.Linear(64 * 14 * 14, 128)
-            self.layer6 = nn.Dropout(p=0.5)
-            self.layer7 = nn.Linear(128, 10)
-
-        def forward(self, x):
-            x = torch.relu(self.layer1(x))
-            x = torch.relu(self.layer2(x))
-            x = torch.relu(self.layer3(x))
-            x = torch.relu(self.layer4(x))
-            x = x.view(x.size(0), -1)
-            x = torch.relu(self.layer5(x))
-            x = torch.relu(self.layer6(x))
-            x = torch.softmax(self.layer7(x), dim=1)
-            return x
-
     model = MnistTrain().to(device)
-    # use cross entropy loss
+
+    # loss function, optimizer 정의
     criterion = param.loss()
     optimizer = param.optimizer(model.parameters(), lr=param.lr)
+    print("loss function: ", criterion)
+    print("optimizer: ", optimizer)
 
     # 학습 루프
     history = []
@@ -125,6 +175,7 @@ for ii in range(3):
         total_loss = 0
         total_accuracy = 0
         for data, target in train_loader:
+            print("\rtrain: " + str(total), end="")
             data = data.to(device)
             target = target.to(device)
 
@@ -187,6 +238,7 @@ for ii in range(3):
         history_test.append(test_loss / total)
         test_accuracy_mean.append(total_accuracy / total)
 
+        print("")
         print(
             f"Epoch: {_epoch+1:2d}, Test Loss: {history_test[-1]:.4f}, Test Accuracy: {test_accuracy_mean[-1]:.4f}, Learning Time: {end_time - start_time:.2f} sec"
         )
